90290b444ee4a8edfaf301d199b9e503f914531f
primayy <primayy@naver.com>
2021-01-19 17:51:53 +0900

    Add GuestbookRepositoryTests for testing the GuestbookRepository

--- guestbook/src/main/java/com/example/guestbook/repository/GuestbookRepository.java
+++ guestbook/src/main/java/com/example/guestbook/repository/GuestbookRepository.java
@@ -1,11 +1,9 @@
-package com.example.repository;
+package com.example.guestbook.repository;
 
 import com.example.guestbook.entity.Guestbook;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.querydsl.QuerydslPredicateExecutor;
 
-public interface GuestbookRepository extends JpaRepository<Guestbook, Long>,
-        QuerydslPredicateExecutor<Guestbook> {
-
+public interface GuestbookRepository extends JpaRepository<Guestbook, Long>, QuerydslPredicateExecutor<Guestbook> {
 
 }
---
+++ guestbook/src/test/java/com/example/guestbook/repository/GuestbookRepositoryTests.java
@@ -0,0 +1,27 @@
+package com.example.guestbook.repository;
+
+import com.example.guestbook.entity.Guestbook;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.stream.IntStream;
+
+@SpringBootTest
+public class GuestbookRepositoryTests {
+
+    @Autowired
+    private GuestbookRepository guestbookRepository;
+
+    @Test
+    public void insertDummies(){
+        IntStream.rangeClosed(1,300).forEach(i->{
+            Guestbook guestbook = Guestbook.builder()
+                    .title("title..."+i)
+                    .content("content..."+i)
+                    .writer("user..."+(i%10))
+                    .build();
+            System.out.println(guestbookRepository.save(guestbook));
+        });
+    }
+}
