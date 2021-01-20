55f93f41610393d13bd5728a0daef279d5fc4e84
primayy <primayy@naver.com>
2021-01-20 14:41:27 +0900

    #8 글 등록 + 등록 처리 후 목록 화면 이동

--- guestbook/src/main/java/com/example/guestbook/controller/GuestbookController.java
+++ guestbook/src/main/java/com/example/guestbook/controller/GuestbookController.java
@@ -1,5 +1,6 @@
 package com.example.guestbook.controller;
 
+import com.example.guestbook.dto.GuestbookDTO;
 import com.example.guestbook.dto.PageRequestDTO;
 import com.example.guestbook.service.GuestbookService;
 import lombok.RequiredArgsConstructor;
@@ -7,7 +8,9 @@ import lombok.extern.log4j.Log4j2;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 @Controller
 @RequestMapping("/guestbook")
@@ -27,5 +30,21 @@ public class GuestbookController {
         log.info("list.........." + pageRequestDTO);
 
         model.addAttribute("result", service.getList(pageRequestDTO));
+    }
+
+    @GetMapping("/register")
+    public void register(){
+        log.info("register........");
+    }
+
+    @PostMapping("/register")
+    public String registerPost(GuestbookDTO dto, RedirectAttributes redirectAttributes){
+        log.info("register dto....." + dto);
+
+        Long gno = service.register(dto);
+
+        redirectAttributes.addFlashAttribute("msg", gno);
+
+        return "redirect:/guestbook/list";
     }
 }
