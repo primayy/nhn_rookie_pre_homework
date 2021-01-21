cc5d3b91453d3fdedc326ae9902f647f467f13f4
primayy <primayy@naver.com>
2021-01-21 21:48:44 +0900

    #13 MovieImage 추가

---
+++ mreview/src/main/java/com/example/mreview/entity/MovieImage.java
@@ -0,0 +1,27 @@
+package com.example.mreview.entity;
+
+import lombok.*;
+
+import javax.persistence.*;
+
+@Entity
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@ToString(exclude = "movie")
+public class MovieImage extends BaseEntity{
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long inum;
+
+    private String uuid;
+
+    private String imgName;
+
+    private String path;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    private Movie movie;
+}
